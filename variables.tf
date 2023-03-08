ecbe323d7f914265f32d34c1dc84fd11406c3bff
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2023-03-08 21:47:38 +0100

    Add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,15 +1,26 @@
 variable "email_from" {
-  type = string
+  description = "The email address of the sender"
+  type        = string
 }
 
 variable "email_to" {
-  type = string
+  description = "The email address of the recipient"
+  type        = string
 }
 
 variable "bucket_name" {
-  type = string
+  description = "The name of the S3 bucket that will trigger the Lambda function"
+  type        = string
 }
 
 variable "kms_key_arn" {
-  type = string
+  description = "The ARN of the KMS key used to encrypt and decrypt sensitive data"
+  type        = string
+  default     = ""
+}
+
+variable "filter_paths" {
+  description = "A set of S3 object key prefixes to filter on. Only objects with keys that match at least one of these prefixes will trigger the Lambda function. Defaults to matching all object keys with '*'."
+  type        = set(string)
+  default     = [""]
 }
